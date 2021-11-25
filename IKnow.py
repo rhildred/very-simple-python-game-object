a547620f679e6944946596b59dab6cc20f1e520d
Richard Hildred <rhildred@gmail.com>
2021-11-25 16:26:12 -0500

    add main guard

--- IKnow.py
+++ IKnow.py
@@ -4,3 +4,10 @@ class IKnow:
     def takeTurn(self, sInput):
         return "I know you are a " + sInput + " but what am I?"
     
+if __name__ == '__main__':
+    oGame = IKnow()
+
+    while True:
+        sInput = input("> ")
+        sReturn = oGame.takeTurn(sInput)
+        print(sReturn)
